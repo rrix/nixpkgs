99eb96e78d9902dd0990bb32685d5fb5f59e7883
Ryan Rix <ryan@whatthefuck.computer>
2022-06-24 13:24:09 -0700

    optionally propagate beets package in beetsPackages.beetcamp
    If beets is propagated while it is acting as a plugin, the parent
    beets' files will not appear in the final package; if beets is not
    propagated, beetcamp cli won't work.
    
    This feels like an anti-pattern to me but I think it works? I'm not
    sure if these packages will conflict in a system profile though.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4511,6 +4511,7 @@ with pkgs;
 
   beetsPackages = lib.recurseIntoAttrs (callPackage ../tools/audio/beets { });
   inherit (beetsPackages) beets beets-unstable;
+  beetcamp = beetsPackages.beetcamp.override({propagateBeets = true;});
 
   bento4 = callPackage ../tools/video/bento4 { };
 
