fe4956d42aafc974e3ba11f3b4d0be983260eeb7
David Arnold <david.arnold@iohk.io>
2023-07-09 18:01:02 -0500

    python3Packages.pydantic-core: init at 2.3.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8351,6 +8351,8 @@ self: super: with self; {
 
   pydantic = callPackage ../development/python-modules/pydantic { };
 
+  pydantic-core = callPackage ../development/python-modules/pydantic-core { };
+
   pydantic-scim = callPackage ../development/python-modules/pydantic-scim { };
 
   pydash = callPackage ../development/python-modules/pydash { };
